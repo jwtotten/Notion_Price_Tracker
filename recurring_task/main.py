9c49f9e83c39b2f0ca5e834aa19dcfe15da510d5
Jack <jacktotten@hotmail.co.uk>
2024-12-03 22:36:05 +0000

    adding requests to collect fact from website in tutorial.

--- recurring_task/main.py
+++ recurring_task/main.py
@@ -9,3 +9,17 @@ load_dotenv()
 
 # load in the NOTION_SECRET from the .env file
 NOTION_TOKEN = os.getenv('NOTION_SECRET', '')
+
+def get_cool_fact():
+    r = requests.get('https://asli-fun-fact-api.herokuapp.com/')
+
+    # Request failed, don't try to parse and just return None
+    if not r.ok:
+        return None
+
+    # Parse JSON
+    json_data = r.json()
+
+    return json_data['data']
+
+print(get_cool_fact())
