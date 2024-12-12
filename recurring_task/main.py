58376acd10c21a9ed9dd5508003c0df0eab054ab
Jack <jacktotten@hotmail.co.uk>
2024-12-12 11:56:10 +0000

    Adding new method for scraping websites to find the price of the item. Still in progress.

--- recurring_task/main.py
+++ recurring_task/main.py
@@ -4,6 +4,7 @@ import time
 import os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+from itertools import cycle
 
 # load in the .env file
 load_dotenv()
@@ -26,5 +27,37 @@ def get_cool_fact():
     if not r.ok:
         print('Failed to make request to selected site.')
         return None
+    
+def get_price(url: str) -> str:
+    headers = {'User -Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    # proxy_pool = cycle(['https://proxy1', 'https://proxy2', 'https://proxy3'])
+    # proxy = next(proxy_pool)
 
-print(get_cool_fact())
+    connection_timeout = 6
+    read_timeout = 10
+
+    for _ in range(3):
+        try:
+            response = requests.get(url=url, headers=headers, timeout=(connection_timeout, read_timeout))
+            # response = requests.get(url=url, headers=headers, timeout=(connection_timeout, read_timeout), proxies={"http": proxy, "https": proxy})
+            print('Response status: %s' % response.status_code)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            price = soup.find('span', {'class', 'a-price-whole'})
+            break
+        except requests.exceptions.ConnectionError as e:
+            response = 'No response from url.'
+            price = None
+            print(e)
+
+    return soup
+
+if __name__ == '__main__':
+    url: str = 'https://thepihut.com/products/raspberry-pi-5?variant=43878483198147'
+    price: str = get_price(url)
+
+    print(f'Price of item: {price}')
+    print('')
+    print(type(price))
+
+    if 'price' in price:
+        print('found something with price in it!')
